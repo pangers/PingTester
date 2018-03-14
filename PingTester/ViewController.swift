a71e62a6ca07a6e44811e02cf38a3148f2c2d228
James Pang <james.pang@tigerspike.com>
2018-03-14 11:39:02 +1100

    Initial commit

---
+++ PingTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PingTester
+//
+//  Created by James Pang on 14/3/18.
+//  Copyright © 2018 Pangers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
