4f10606e5214bc9bffdf041d202aa2580aecd30f
James Pang <james.pang@tigerspike.com>
2018-03-14 12:13:48 +1100

    Working Pin tester

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+ruby '2.2.0'
+
+gem 'cocoapods', '1.4.0'
--- PingTester/ViewController.swift
+++ PingTester/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import PlainPing
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let tap = UITapGestureRecognizer()
+        tap.addTarget(self, action: #selector(viewTapped))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func viewTapped() {
+        view.endEditing(false)
     }
-
-
+    
+    @IBAction func pingTapped(_ sender: Any) {
+        resultLabel.text = ""
+        let hostname = textField.text ?? ""
+        let pingText = "Pinging: \(hostname)\n"
+        print(pingText)
+        resultLabel.text = currentResultLabelText() + pingText
+        PlainPing.ping(hostname, withTimeout: 2) { [unowned self] (timeElapsed, error) in
+            if let error = error {
+                let errorText = "error: \(error.localizedDescription)\n"
+                print(errorText)
+                self.resultLabel.text = self.currentResultLabelText() + errorText
+            }
+            
+            if let latency = timeElapsed {
+                let latencyText = "Latency (ms): \(latency)\n"
+                print(latencyText)
+                self.resultLabel.text = self.currentResultLabelText() + latencyText
+            }
+        }
+    }
+    
+    func currentResultLabelText() -> String {
+        return resultLabel.text ?? ""
+    }
+    
 }
 
